a05c64aad0e6613c90b427cbed3d43a734ad4ec3
paoli7612 <paoli7612@gmail.com>
2021-07-26 22:02:31 +0200

    see ingredients of any drink

--- resources/views/drinks.blade.php
+++ resources/views/drinks.blade.php
@@ -6,8 +6,17 @@
         <div class="card-header">{{ __('Drinks') }}</div>
         <div class="card-body">
             @foreach ($drinks as $drink)
-                <div class="alert alert-success" role="alert">
-                    {{ $drink->name }}
+                <div class="card">
+                    <div class="card-header">
+                        {{ $drink->name }}
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            @foreach ($drink->ingredients as $ingredient)
+                                <li>{{ $ingredient->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             @endforeach
         </div>
