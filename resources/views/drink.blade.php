732cc377931e07e3c36c8686407aa8d72a4e8d8f
paoli7612 <paoli7612@gmail.com>
2021-07-29 07:07:39 +0200

    form aggiungi ingrediente

--- resources/views/drink.blade.php
+++ resources/views/drink.blade.php
@@ -4,4 +4,29 @@
 
     <h1>{{ $drink->name }}</h1>
 
+    <ul>
+
+        @foreach ($drink->ingredients as $ingredient)
+            <li>{{ $ingredient->name }}</li>
+        @endforeach
+
+    </ul>
+
+    <form action="{{ $drink->route }}" method="post">
+        @csrf
+        @method('put')
+
+        <div class="form-group">
+            <div class="row mx-0">
+                <select name="ingredient_id" class="form-control col-sm-6 mb-3 mr-sm-3">
+                    @foreach (App\Models\Ingredient::all() as $ingredient)
+                        <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                    @endforeach
+                </select>
+                <input type="submit" class="btn btn-primary col h-100" value="new drink">
+            </div>
+        </div>
+    </form>
+
+
 @endsection
