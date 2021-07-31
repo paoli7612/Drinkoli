c915929f8d6cbffb605cc212316070f31209b965
Tommaso Paoli <paoli7612@gmail.com>
2021-07-31 21:19:43 +0200

    heade in new file

--- views/layouts/page_start.php
+++ views/layouts/page_start.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link href="/awesome-icons\css\all.css" rel="stylesheet">
-    <title>Drinkoli - <?= $title ?></title>
-    <link rel="stylesheet" href="/style/main.css">
+    <?php include 'head.php'?>
 </head>
 <body>
     <?php include 'header.php'?>
