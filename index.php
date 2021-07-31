9aba0fe0601bd0fcaf05003753ebf24f45f49945
Tommaso Paoli <paoli7612@gmail.com>
2021-07-31 15:57:18 +0200

    foreach in nav

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
     include 'core/Request.php';
     include 'core/Router.php';
 
+    $nav = [
+        [
+            'link' => '/',
+            'icon' => 'fa fa-home',
+            'title' => 'Home'
+        ],
+        [
+            'link' => '/contatti',
+            'icon' => 'fa fa-contact',
+            'title' => 'Contatti'
+        ],
+    ];
+
     $r = new Router;
     $r->get('', 'views/home.php');
     require $r->direct(Request::uri(), Request::method());
