ade2fbb0528b78fc3e222e01c2b8bd42fa622ce4
Tommaso Paoli <paoli7612@gmail.com>
2021-08-01 10:27:37 +0200

    resposnive mobile

--- core/partials/navbar-item.php
+++ core/partials/navbar-item.php
@@ -1,4 +1,6 @@
 <a class="w3-btn w3-bar-item <?= (Request::uri() == $item->link) ? 'w3-white' : '' ?>" href="/<?= $item->link ?>">
-    <?= $item->title ?>
+    <span class="w3-hide-small">
+        <?= $item->title ?>
+    </span>    
     <i class="<?= $item->icon ?> fa-lg"></i>
 </a>
