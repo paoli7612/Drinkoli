1c3374798e0b4f63da1868d81ad1c1abaa267ab2
Tommaso Paoli <paoli7612@gmail.com>
2021-08-01 16:21:42 +0200

    login form

--- views/auth/sing-in.view.php
+++ views/auth/sing-in.view.php
@@ -1,5 +1,15 @@
 <?php $title = 'Sing in' ?>
 <?php include 'views/layouts/page_start.php' ?>
 
+<div class="w3-panel w3-third">
+    <div class="w3-panel w3-green w3-card-4 w3-round-large">
+        <div class="w3-panel">
+            <form action="login" method="post">
+                <input class="w3-input w3-round-large" type="text" placeholder="nickname" required autofocus>
+                <input class="w3-input w3-round-large" type="text" placeholder="password" required autofocus>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php include 'views/layouts/page_end.php' ?>
