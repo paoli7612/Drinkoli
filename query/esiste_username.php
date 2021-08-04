8eb727e93f3c2cd2dda62771955540124b2ab091
Tommaso Paoli <paoli7612@gmail.com>
2021-08-04 17:11:56 +0200

    query username gia esistente

---
+++ query/esiste_username.php
@@ -0,0 +1,12 @@
+<?php
+    chdir('../');
+    include 'bootstrap.php';
+
+    App::init();
+    Database::init();
+    Auth::init();
+
+    if (empty(Database::select_where('users', 'User', "`username` = '".$_GET['username']."';")))
+        echo 0;
+    else echo 1;
+
