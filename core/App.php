e92a6eff81b245a1d194fda5a3574c6991531680
paoli7612 <paoli7612@gmail.com>
2021-08-05 15:15:08 +0200

    themes var not necessary

--- core/App.php
+++ core/App.php
@@ -1,8 +1,8 @@
 
 <?php
 
-    class App {
-
+    class App
+    {
         public static $config;
 
         public static function init()
@@ -12,7 +12,6 @@
 
         public static function themes()
         {
-            return $themes = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'khaki', 'yellow', 'amber', 'orange', 'deep-orange', 'blue-grey', 'brown', 'grey', 'dark-grey', 'black'];
+            return ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'khaki', 'yellow', 'amber', 'orange', 'deep-orange', 'blue-grey', 'brown', 'grey', 'dark-grey', 'black'];
         }
-
     }
