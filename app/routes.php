ca2568702bfe7b9d041a6885ffb7bd3fbb013c71
paoli7612 <paoli7612@gmail.com>
2021-08-07 10:57:10 +0200

    views of ingredients and drinks in a folder

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,7 @@
 use App\core\Route;
 
     Route::get('', 'home');
-    Route::get('drinks', 'drinks');
-    Route::get('ingredients', 'ingredients');
+    Route::get('drinks', 'drink/all');
+    Route::get('ingredients', 'ingredient/all');
     Route::get('enter', 'enter');
     Route::get('settings', 'settings');
